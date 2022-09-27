db2a716e45eff157d5f1bb9d203acce5f97e5bc4
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-09-27 10:40:05 +0530

    Added : Compare Length Based on End Point Program

--- src/com/program/lincomparision/LineComparision.java
+++ src/com/program/lincomparision/LineComparision.java
@@ -1,31 +1,32 @@
 package com.program.lincomparision;
-
 public class LineComparision {
-	public static double CheckoutLength(int x1,int x2,int y1,int y2) {     
-		int num=((x2 - x1)*(x2 - x1)) + ((y2- y1)*(y2- y1));
-		double length=Math.sqrt(num);
-		return length;
-		}
-	public static void checkEquality() {
-	// CALCULATE LENGTH
-	double length1= CheckoutLength(1,20,5,2);
-	double length= CheckoutLength(12,10,12,1);
-
-	// CHECK THE EQUAL OR UNEQUAL LENGTH
-	if (length==length1) {
-		System.out.println("The Length of The Two Lines is Equal");
-	}
-	else {
-		System.out.println("The Length of The Two Lines is UnEqual");
-	}
-	System.out.println("The Length of The Line is  " + length1 );
-	System.out.println("The Length of The Line is  " + length );
-	}
 	
-	public static void main(String args []) {
-		checkEquality();
-	}
+public static double checkoutLength(int x1,int x2,int y1,int y2) {     
+	int num=((x2 - x1)*(x2 - x1)) + ((y2- y1)*(y2- y1));
+	double length=Math.sqrt(num);
+	return length;
 }
 
+public static void checkLineLength() {
+	//OBJECT-1
+	double length1=checkoutLength(1,20,5,2);
 
+	// OBJECT-2
+	double length2=checkoutLength(12,10,12,1);
 
+	// CHECKING THE LENGTH IS EQUAL,GREATER OR LESS
+	if (length2==length1) {
+		System.out.println("The Two Length is Equal");
+	}
+	else if (length2>length1) {
+		System.out.println("The Length First is Less Than Length Second");
+	}
+	else if (length2<length1) {
+		System.out.println("The Length First is Greater Than Length Second");
+	}
+}
+public static void main(String args[]) {
+	checkLineLength();
+}
+
+}
