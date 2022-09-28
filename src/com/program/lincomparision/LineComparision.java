4b0aa17d52c34c45f63be5711413230a595a7cc7
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-09-28 10:06:12 +0530

    Added : Solve Opps Concept & Compare Length in JavaCompareMethod Program

--- src/com/program/lincomparision/LineComparision.java
+++ src/com/program/lincomparision/LineComparision.java
@@ -1,32 +1,33 @@
 package com.program.lincomparision;
 public class LineComparision {
 	
-public static double checkoutLength(int x1,int x2,int y1,int y2) {     
+public static int checkoutLength(int x1,int x2,int y1,int y2) {     
 	int num=((x2 - x1)*(x2 - x1)) + ((y2- y1)*(y2- y1));
 	double length=Math.sqrt(num);
-	return length;
+	return (int) length;
 }
 
 public static void checkLineLength() {
 	//OBJECT-1
-	double length1=checkoutLength(1,20,5,2);
+	int length1=checkoutLength(1,20,5,2);
 
 	// OBJECT-2
-	double length2=checkoutLength(12,10,12,1);
-
+	int length2=checkoutLength(12,10,12,1);
+	
 	// CHECKING THE LENGTH IS EQUAL,GREATER OR LESS
-	if (length2==length1) {
+	int result=Integer.compare(length1, length2);  // Compare To Method 
+	
+	if (result==0) {
 		System.out.println("The Two Length is Equal");
 	}
-	else if (length2>length1) {
+	else if (-1==result) {
 		System.out.println("The Length First is Less Than Length Second");
 	}
-	else if (length2<length1) {
+	else if (1==result) {
 		System.out.println("The Length First is Greater Than Length Second");
 	}
 }
 public static void main(String args[]) {
 	checkLineLength();
 }
-
 }
